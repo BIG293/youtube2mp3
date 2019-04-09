d9d5039c166168322d7bb58b2531ed3f24ab0fa1
simone <simo.big2@gmail.com>
2019-04-09 19:10:20 +0200

    first commit

---
+++ ytdownload.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+YLD_OPTIONS = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+def download_songs():
+    file_list = open('to-download.txt', 'r')
+
+    with youtube_dl.YoutubeDL(YLD_OPTIONS) as ydl:
+        url_list = []
+        for url in file_list:
+            url_list.append(url)
+        ydl.download(url_list)
+
+download_songs()
